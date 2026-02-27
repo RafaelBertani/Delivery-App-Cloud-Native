0b36dfade91c10e54e86705c64b42b0b346b9a09
RafaelBertani <Rafaelm.bertani@gmail.com>
2026-02-27 00:57:51 -0300

    fix(infra): repair database creation script

--- infra/bin/create-db.sh
+++ infra/bin/create-db.sh
@@ -1,27 +1,45 @@
+# set -e
+
+# STACK_NAME="delivery_app"
+# SERVICE_NAME="postgres"
+
+# echo "🔍 Procurando container do Postgres..."
+
+# POSTGRES_CONTAINER=$(docker ps \
+#   --filter "name=${STACK_NAME}_${SERVICE_NAME}" \
+#   --format "{{.Names}}" \
+#   | head -n 1)
+
+# if [ -z "$POSTGRES_CONTAINER" ]; then
+#   echo "❌ Container do Postgres não encontrado"
+#   exit 1
+# fi
+
+# echo "✅ Container encontrado: $POSTGRES_CONTAINER"
+# echo "🚀 Executando script SQL..."
+
+# docker exec -i "$POSTGRES_CONTAINER" \
+#   psql -U postgres < "$(dirname "$0")/../db/init.sql"
+
+# echo "🎉 Banco e tabelas criados com sucesso"
+
+# echo "⏳ Aguardando 10 segundos..."
+# sleep 10
+
 set -e
 
-STACK_NAME="delivery_app"
-SERVICE_NAME="postgres"
+NAMESPACE="delivery-app"
+DEPLOYMENT="deployment/postgres"
 
-echo "🔍 Procurando container do Postgres..."
+echo "🔍 Conectando ao Postgres no Kubernetes..."
 
-POSTGRES_CONTAINER=$(docker ps \
-  --filter "name=${STACK_NAME}_${SERVICE_NAME}" \
-  --format "{{.Names}}" \
-  | head -n 1)
-
-if [ -z "$POSTGRES_CONTAINER" ]; then
-  echo "❌ Container do Postgres não encontrado"
-  exit 1
-fi
-
-echo "✅ Container encontrado: $POSTGRES_CONTAINER"
+# No K8s, não precisa caçar o ID do container, o kubectl exec faz isso
 echo "🚀 Executando script SQL..."
 
-docker exec -i "$POSTGRES_CONTAINER" \
-  psql -U postgres < "$(dirname "$0")/../db/init.sql"
+kubectl exec -i $DEPLOYMENT -n $NAMESPACE -- psql -U postgres < "$(dirname "$0")/../db/init.sql"
 
-echo "🎉 Banco e tabelas criados com sucesso"
+echo "🎉 Banco e tabelas criados com sucesso!"
+echo "⏳ Aguardando 5 segundos para estabilizar..."
+sleep 5
 
-echo "⏳ Aguardando 10 segundos..."
-sleep 10
+
